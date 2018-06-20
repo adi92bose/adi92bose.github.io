c51458d0d7a5109cbfb5956a26746bc4335dacbb
Shiladitya Bose <adi123igate@gmail.com>
2018-06-20 07:26:33 +0530

    Add files via upload

---
+++ modal/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+ $('#myBtn').click(function(){
+ 	$('#myModal').css('display','block')
+ });
+
+ $('.close').click(function(){
+ 	$('#myModal').css('display','none')
+ })
+
+ $('#savebtn').click(function(){
+ 	$('#myModal').css('display','none')
+ })
+
+ $('#closebtn').click(function(){
+ 	$('#myModal').css('display','none')
+ })
+
+
+})
+
